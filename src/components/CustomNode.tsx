00c3ae74c878ede7afd8cae80bcaa77240c041da
Akbar Idria <akbaridria15@gmail.com>
2024-01-29 12:35:54 +0700

    change background chain logo

--- src/components/CustomNode.tsx
+++ src/components/CustomNode.tsx
@@ -8,7 +8,7 @@ import datas from '@/data.json';
 const CustomNode = ({ data } : NodeProps<NodeData>) => {
   return (
     <div className='flex flex-col gap-1 p-3 w-[150px] justify-center items-center rounded-lg border-[1px] border-tremor-brand text-sm bg-transparent hover:bg-tremor-brand-faint transition-all'>
-      <div className={`bg-[${data.background}] rounded-full w-4 h-4`}>
+      <div className={`bg-[black] rounded-full w-4 h-4`}>
         <img src={data.chainLogo} alt="" className='w-4 h-4' />
       </div>
       <div>{data.label}</div>
